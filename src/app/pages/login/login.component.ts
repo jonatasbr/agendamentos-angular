d1521f4f655fa9b6b1b5567fdffcdd12d04e2aaf
Jonatas Sousa <jonatasbraz@gmail.com>
2024-06-18 16:06:04 -0300

    layout do form

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
+import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
+interface LoginForm {
+  email: FormControl,
+  password: FormControl
+}
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ DefaultLoginLayoutComponent ],
+  imports: [
+    DefaultLoginLayoutComponent,
+    ReactiveFormsModule,
+    PrimaryInputComponent,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm!: FormGroup<LoginForm>;
 
 }
